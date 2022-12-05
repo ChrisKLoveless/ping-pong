1e6228a0f6d33fc2b2005bb4387993191f359e8b
Chris Loveless <chriskloveless@gmail.com>
2022-12-05 13:33:31 -0800

    add logic to convert user input

--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
 using System;
 using System.Collections.Generic;
-using Dealership.Models;
+
+
+public class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Input a number: ");
+    string stringInput = Console.ReadLine();
+    int numInput = int.Parse(stringInput);
+
+    for (int i = 1; i <= numInput; i++)
+    {
+      if(i % 3 == 0 && i % 5 == 0)
+      {
+        Console.WriteLine("Ping-Pong!");
+      }
+      else if(i % 3 == 0)
+      {
+        Console.WriteLine("Ping!");
+      }
+      else if(i % 5 == 0)
+      {
+        Console.WriteLine("Pong!");
+      }
+      else
+      {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
